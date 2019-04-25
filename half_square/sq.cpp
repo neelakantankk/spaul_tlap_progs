e3245df024f8fd9a750ca18d1227d4fd5f73fc3e
Neelakantan K.K <neelakantankk@gmail.com>
2019-04-25 15:16:50 +0530

    First Commit

---
+++ half_square/sq.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main() {
+    for (int i=5;i>0;i--) {
+        for(int j=0;j<i;j++) {
+            cout<<"#";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
